fd1c0dbf4d7ca3033fbff39d91141aebaae22a29
Plenluno <plenluno@zoho.com>
2012-05-20 14:13:37 +0900

    introduce Status

--- src/error.cpp
+++ src/error.cpp
@@ -1,63 +1,39 @@
 // Copyright (c) 2012 Plenluno All rights reserved.
 
 #include "libj/error.h"
-#include "libj/string.h"
 
 namespace libj {
 
 class ErrorImpl : public Error {
- public:
-    Code code() const {
-        return code_;
-    }
-    
-    String::CPtr toString() const {
-        return String::create();
-    }
-    
- public:
-    static CPtr create() {
-        CPtr p(new ErrorImpl());
-        return p;
-    }
-
-    static CPtr create(Code code) {
-        CPtr p(new ErrorImpl(code));
-        return p;
-    }
-    
-    static CPtr create(Code code, String::CPtr msg) {
-        Error::CPtr p(new ErrorImpl(code, msg));
-        return p;
-    }
-    
  private:
-    Code code_;
-    String::CPtr message_;
+    Status::CPtr status_;
     
-    ErrorImpl()
-        : code_(Error::ANY)
-        , message_(static_cast<String*>(0)) {}
+    ErrorImpl(Int code)
+        : status_(Status::create(code)) {}
     
-    ErrorImpl(Code code)
-        : code_(code)
-        , message_(static_cast<String*>(0)) {}
-    
-    ErrorImpl(Code code, String::CPtr msg)
-        : code_(code)
-        , message_(msg) {}
+    ErrorImpl(Int code, String::CPtr msg)
+        : status_(Status::create(code, msg)) {}
+ 
+ public:
+     static CPtr create(Int code) {
+         CPtr p(new ErrorImpl(code));
+         return p;
+     }
+     
+     static CPtr create(Int code, String::CPtr msg) {
+         CPtr p(new ErrorImpl(code, msg));
+         return p;
+     }
+     
+     LIBJ_STATUS_IMPL(status_);
 };
 
-Error::CPtr Error::create() {
-    return ErrorImpl::create();
-}
-
-Error::CPtr Error::create(Code code) {
+Error::CPtr Error::create(Int code) {
     return ErrorImpl::create(code);
 }
 
-Error::CPtr Error::create(Code code, String::CPtr msg) {
+Error::CPtr Error::create(Int code, String::CPtr msg) {
     return ErrorImpl::create(code, msg);
 }
 
-}  // namespace libj
+}
---
+++ src/status.cpp
@@ -0,0 +1,53 @@
+// Copyright (c) 2012 Plenluno All rights reserved.
+
+#include "libj/status.h"
+#include "libj/string.h"
+
+namespace libj {
+
+class StatusImpl : public Status {
+ public:
+    Int code() const {
+        return code_;
+    }
+    
+    String::CPtr toString() const {
+        if (message_)
+            return message_;
+        else
+            return String::create();
+    }
+    
+ public:
+    static CPtr create(Int code) {
+        CPtr p(new StatusImpl(code));
+        return p;
+    }
+    
+    static CPtr create(Int code, String::CPtr msg) {
+        Status::CPtr p(new StatusImpl(code, msg));
+        return p;
+    }
+    
+ private:
+    Int code_;
+    String::CPtr message_;
+    
+    StatusImpl(Int code)
+        : code_(code)
+        , message_(static_cast<String*>(0)) {}
+    
+    StatusImpl(Int code, String::CPtr msg)
+        : code_(code)
+        , message_(msg) {}
+};
+
+Status::CPtr Status::create(Int code) {
+    return StatusImpl::create(code);
+}
+
+Status::CPtr Status::create(Int code, String::CPtr msg) {
+    return StatusImpl::create(code, msg);
+}
+
+}  // namespace libj
