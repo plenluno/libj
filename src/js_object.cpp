05d21f8af37887e9adc29c8f8038d9e782ca5a93
Plenluno <plenluno@zoho.com>
2012-05-20 20:10:46 +0900

    move JsObject from libnode to libj

---
+++ src/js_object.cpp
@@ -0,0 +1,53 @@
+// Copyright (c) 2012 Plenluno All rights reserved.
+
+#include "libj/js_object.h"
+
+namespace libj {
+
+class JsObjectImpl : public JsObject {
+ public:
+    static Ptr create() {
+        Ptr p(new JsObjectImpl());
+        return p;
+    }
+    
+    Value put(const Value& key, const Value& val) {
+        return map_->put(String::valueOf(key), val);
+    }
+    
+    String::CPtr toString() const {
+        return map_->toString();
+    }
+    
+    Size size() const {
+        return map_->size();
+    }
+    
+    Value get(const Value& key) const {
+        return map_->get(key);
+    }
+    
+    Value remove(const Value& key) {
+        return map_->remove(key);
+    }
+    
+    Set::CPtr keySet() const {
+        return map_->keySet();
+    }
+    
+    void clear() {
+        map_->clear();
+    }
+
+ private:
+    Map::Ptr map_;
+
+    JsObjectImpl() : map_(Map::create()) {
+    }
+};
+
+JsObject::Ptr JsObject::create() {
+    return JsObjectImpl::create();
+}
+
+}  // namespace libj
