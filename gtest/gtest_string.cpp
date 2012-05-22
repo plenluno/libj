1dff41953bfbcb07954720ed6c5f6ea077914d17
Kota Handa <pxn07044@nifty.ne.jp>
2012-05-23 00:35:25 +0900

    add test for String:create

--- gtest/gtest_string.cpp
+++ gtest/gtest_string.cpp
@@ -11,6 +11,43 @@ TEST(GTestString, TestCreate) {
     ASSERT_EQ(s1->compareTo(s2), 0);
 }
 
+TEST(GTestString, TestCreateUtf) {
+    const int8_t  a8[]  = "abcde";
+    const int16_t a16[] = {0x61, 0x62, 0x63, 0x64, 0x65, 0};
+    const int32_t a32[] = {0x61, 0x62, 0x63, 0x64, 0x65, 0};
+    String::CPtr s1 = String::create(a8, String::ASCII);
+    String::CPtr s2 = String::create(a8, String::UTF8);
+    String::CPtr s3 = String::create(a16, String::UTF16);
+    String::CPtr s4 = String::create(a32, String::UTF32);
+    ASSERT_EQ(s1->compareTo(s2), 0);
+    ASSERT_EQ(s2->compareTo(s3), 0);
+    ASSERT_EQ(s3->compareTo(s4), 0);
+
+    const int8_t u8[] = {
+        0x01,                   // U+0001
+        0x7f,                   // U+007F
+        0xc2, 0x80,             // U+0080
+        0xdf, 0xbf,             // U+07FF
+        0xe0, 0xa0, 0x80,       // U+0800
+        0xef, 0xbf, 0xbf,       // U+FFFF
+        0xf0, 0x90, 0x80, 0x80, // U+10000
+        0xf4, 0x8f, 0xbf, 0xbf  // U+10FFFF
+    };
+    const int16_t u16[] = {
+        0x01, 0x7f, 0x0080, 0x07ff, 0x0800, 0xffff,
+        0xd800, 0xdc00, // U+10000
+        0xdbff, 0xdfff  // U+10FFFF
+    };
+    const int32_t u32[] = {
+        0x01, 0x7f, 0x0080, 0x07ff, 0x0800, 0xffff, 0x10000, 0x10ffff
+    };
+    String::CPtr s5 = String::create(u8, String::UTF8);
+    String::CPtr s6 = String::create(u16, String::UTF16);
+    String::CPtr s7 = String::create(u32, String::UTF32);
+    ASSERT_EQ(s5->compareTo(s6), 0);
+    ASSERT_EQ(s6->compareTo(s7), 0);
+}
+
 TEST(GTestString, TestInstanceOf) {
     String::CPtr s = String::create();
     ASSERT_TRUE(s->instanceOf(Type<String>::id()));
