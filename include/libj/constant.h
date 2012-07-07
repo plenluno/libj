705a955f6a8175a8b4a03bbd4cac4f4e71dc34b6
Plenluno <plenluno@zoho.com>
2012-07-07 20:54:21 +0900

    add constant.h and constant.cpp

---
+++ include/libj/constant.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2012 Plenluno All rights reserved.
+
+#ifndef LIBJ_CONSTANT_H_
+#define LIBJ_CONSTANT_H_
+
+#include "libj/typedef.h"
+
+namespace libj {
+
+extern const Size NO_POS;
+extern const Char NO_CHAR;
+
+}  // namespace libj
+
+#endif  // LIBJ_CONSTANT_H_
---
+++ src/constant.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2012 Plenluno All rights reserved.
+
+#include "libj/constant.h"
+
+namespace libj {
+
+const Size NO_POS = -1;
+const Char NO_CHAR = -1;
+
+}  // namespace libj
