f07d1b84469a79def25576ba1d63601fb5f44bbe
Plenluno <plenluno@zoho.com>
2012-08-29 23:08:01 +0900

    modify console::log output in the case of Map and Collection instances

--- src/console.cpp
+++ src/console.cpp
@@ -2,19 +2,29 @@
 
 #include <iostream>
 
+#include "libj/collection.h"
 #include "libj/console.h"
 #include "libj/json.h"
+#include "libj/map.h"
 #include "libj/string.h"
 
 namespace libj {
 namespace console {
 
 Boolean log(const Value& val) {
-    String::CPtr s = String::valueOf(val);
-    if (!s)
+    String::CPtr s = String::null();
+    if (val.instanceof(Type<Map>::id()) ||
+        val.instanceof(Type<Collection>::id())) {
+        s = json::stringify(val);
+    } else {
+        s = String::valueOf(val);
+    }
+    if (s) {
+        std::cout << s->toStdString() << std::endl;
+        return true;
+    } else {
         return false;
-    std::cout << s->toStdString() << std::endl;
-    return true;
+    }
 }
 
 }  // namespace console
