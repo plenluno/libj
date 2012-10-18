ddcccf97c7a0e6cc43bf6df031015a50fdcdeef3
Plenluno <plenluno@zoho.com>
2012-10-18 22:54:16 +0900

    introduce TypedValueHolder

---
+++ gtest/gtest_typed_value_holder.cpp
@@ -0,0 +1,70 @@
+// Copyright (c) 2012 Plenluno All rights reserved.
+
+#include <gtest/gtest.h>
+#include <libj/array_list.h>
+#include <libj/typed_value_holder.h>
+
+namespace libj {
+
+TEST(GTestTypedValueHolder, TestCreate) {
+    TypedValueHolder<Int>::Ptr h = TypedValueHolder<Int>::create(1);
+    ASSERT_TRUE(!!h);
+
+    TypedValueHolder<String::CPtr>::Ptr h2 =
+        TypedValueHolder<String::CPtr>::create(String::create("123"));
+    ASSERT_TRUE(!!h2);
+}
+
+TEST(GTestTypedValueHolder, TestGet) {
+    TypedValueHolder<Int>::Ptr h = TypedValueHolder<Int>::create(3);
+    ASSERT_EQ(3, h->get());
+}
+
+TEST(GTestTypedValueHolder, TestGetPtr) {
+    ArrayList::Ptr a = ArrayList::create();
+    TypedValueHolder<ArrayList::Ptr>::Ptr h =
+        TypedValueHolder<ArrayList::Ptr>::create(a);
+    ASSERT_TRUE(h->get()->equals(a));
+
+    ASSERT_TRUE(a->isEmpty());
+    h->get()->add(5);
+    ASSERT_TRUE(a->get(0).equals(5));
+}
+
+TEST(GTestTypedValueHolder, TestGetCPtr) {
+    String::CPtr s = String::create("abc");
+    TypedValueHolder<String::CPtr>::Ptr h =
+        TypedValueHolder<String::CPtr>::create(s);
+    ASSERT_TRUE(h->get()->equals(s));
+}
+
+TEST(GTestTypedValueHolder, TestSet) {
+    String::CPtr s = String::create("abc");
+    TypedValueHolder<String::CPtr>::Ptr h =
+        TypedValueHolder<String::CPtr>::create(s);
+
+    h->set(h->get()->concat(String::create("123")));
+    ASSERT_TRUE(h->get()->equals(String::create("abc123")));
+}
+
+class GTestTypedValueHolderIncrement {
+ public:
+    TypedValueHolder<Int>::Ptr holder;
+
+    void inc() {
+        holder->set(holder->get() + 1);
+    }
+};
+
+TEST(GTestTypedValueHolder, TestSet2) {
+    GTestTypedValueHolderIncrement i1;
+    GTestTypedValueHolderIncrement i2;
+    TypedValueHolder<Int>::Ptr h = TypedValueHolder<Int>::create(0);
+    i1.holder = h;
+    i2.holder = h;
+    i1.inc();
+    i2.inc();
+    ASSERT_EQ(2, h->get());
+}
+
+}  // namespace libj
---
+++ include/libj/typed_value_holder.h
@@ -0,0 +1,37 @@
+// Copyright (c) 2012 Plenluno All rights reserved.
+
+#ifndef LIBJ_TYPED_VALUE_HOLDER_H_
+#define LIBJ_TYPED_VALUE_HOLDER_H_
+
+#include "libj/mutable.h"
+
+namespace libj {
+
+template<typename T>
+class TypedValueHolder : LIBJ_MUTABLE_TEMPLATE(TypedValueHolder)
+ public:
+    static Ptr create(const T& val) {
+        return Ptr(new TypedValueHolder(val));
+    }
+
+    virtual T get() const {
+        return val_;
+    }
+
+    virtual void set(const T& val) {
+        val_ = val;
+    }
+
+    virtual String::CPtr toString() const {
+        return String::valueOf(val_);
+    }
+
+ private:
+    T val_;
+
+    TypedValueHolder(const T& val) : val_(val) {}
+};
+
+}  // namespace libj
+
+#endif  // LIBJ_TYPED_VALUE_HOLDER_H_
