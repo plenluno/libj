9042c919b5b10ddb1b7db8bd7090b4e9b32f8a8b
Plenluno <plenluno@zoho.com>
2012-10-18 22:52:35 +0900

    refactor ValueHolder

--- include/libj/value_holder.h
+++ include/libj/value_holder.h
@@ -10,10 +10,21 @@ namespace libj {
 
 class ValueHolder : LIBJ_MUTABLE(ValueHolder)
  public:
-    static Ptr create(const Value& v);
+    static Ptr create(const Value& val) {
+        return Ptr(new ValueHolder(val));
+    }
 
-    virtual Value get() const = 0;
-    virtual void set(const Value& val) = 0;
+    virtual Value get() const {
+        return val_;
+    }
+
+    virtual void set(const Value& val) {
+        val_ = val;
+    }
+
+    virtual String::CPtr toString() const {
+        return String::valueOf(val_);
+    }
 
     template<typename T>
     typename Type<T>::Ptr getPtr() const {
@@ -24,6 +35,11 @@ class ValueHolder : LIBJ_MUTABLE(ValueHolder)
     typename Type<T>::CPtr getCPtr() const {
         return toCPtr<T>(get());
     }
+
+ private:
+    Value val_;
+
+    ValueHolder(const Value& val) : val_(val) {}
 };
 
 }  // namespace libj
