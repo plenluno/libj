af02e07f0f423c0ef8c7df1719c7e9111f338da6
Plenluno <plenluno@zoho.com>
2012-11-24 23:59:51 +0900

    introduce detail::LinkedList

---
+++ include/libj/detail/linked_list.h
@@ -0,0 +1,32 @@
+// Copyright (c) 2012 Plenluno All rights reserved.
+
+#ifndef LIBJ_DETAIL_LINKED_LIST_H_
+#define LIBJ_DETAIL_LINKED_LIST_H_
+
+#include "libj/linked_list.h"
+
+#include "./generic_linked_list.h"
+
+namespace libj {
+namespace detail {
+
+template<typename I>
+class LinkedList : public GenericLinkedList<Value, I> {
+ public:
+    virtual Boolean add(const Value& v) {
+        return this->addTyped(v);
+    }
+
+    virtual Boolean add(Size i, const Value& v) {
+        return this->addTyped(i, v);
+    }
+
+    virtual Boolean set(Size i, const Value& v) {
+        return this->setTyped(i, v);
+    }
+};
+
+}  // namespace detail
+}  // namespace libj
+
+#endif  // LIBJ_DETAIL_ARRAY_LIST_H_
--- src/linked_list.cpp
+++ src/linked_list.cpp
@@ -1,33 +1,11 @@
 // Copyright (c) 2012 Plenluno All rights reserved.
 
-#include "libj/linked_list.h"
-#include "libj/detail/generic_linked_list.h"
+#include "libj/detail/linked_list.h"
 
 namespace libj {
 
-typedef detail::GenericLinkedList<Value, LinkedList> ValueLinkedList;
-
-class LinkedListImpl : public ValueLinkedList {
- public:
-    static Ptr create() {
-        return Ptr(new LinkedListImpl());
-    }
-
-    Boolean add(const Value& v) {
-        return ValueLinkedList::addTyped(v);
-    }
-
-    Boolean add(Size i, const Value& v) {
-        return ValueLinkedList::addTyped(i, v);
-    }
-
-    Boolean set(Size i, const Value& v) {
-        return ValueLinkedList::setTyped(i, v);
-    }
-};
-
 LinkedList::Ptr LinkedList::create() {
-    return LinkedListImpl::create();
+    return Ptr(new detail::LinkedList<libj::LinkedList>());
 }
 
 }  // namespace libj
