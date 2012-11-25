1140b0538702e98ff357cdeb3c9bb707f26f00e9
Plenluno <plenluno@zoho.com>
2012-11-25 18:23:04 +0900

    introduce detail::Status

---
+++ include/libj/detail/status.h
@@ -0,0 +1,42 @@
+// Copyright (c) 2012 Plenluno All rights reserved.
+
+#ifndef LIBJ_DETAIL_STATUS_H_
+#define LIBJ_DETAIL_STATUS_H_
+
+#include "libj/symbol.h"
+
+namespace libj {
+namespace detail {
+
+template<typename I>
+class Status : public I {
+ public:
+ 	Status(Int code, String::CPtr msg)
+        : code_(code)
+        , message_(msg) {}
+
+    virtual Int code() const {
+        return code_;
+    }
+
+    virtual String::CPtr message() const {
+        if (message_) {
+            return message_;
+        } else {
+            return String::create();
+        }
+    }
+
+    virtual String::CPtr toString() const {
+        return message();
+    }
+
+ private:
+    Int code_;
+    String::CPtr message_;
+};
+
+}  // namespace detail
+}  // namespace libj
+
+#endif  // LIBJ_DETAIL_STATUS_H_
--- src/status.cpp
+++ src/status.cpp
@@ -1,58 +1,24 @@
 // Copyright (c) 2012 Plenluno All rights reserved.
 
 #include "libj/status.h"
-#include "libj/string.h"
+#include "libj/detail/status.h"
 
 namespace libj {
 
-class StatusImpl : public Status {
- public:
-    Int code() const {
-        return code_;
-    }
-
-    String::CPtr message() const {
-        if (message_) {
-            return message_;
-        } else {
-            return String::create();
-        }
-    }
-
-    String::CPtr toString() const {
-        return message();
-    }
-
- public:
-    static CPtr create(Int code) {
-        String::CPtr msg;
-        if (code == OK) {
-            msg = String::create("OK");
-        } else {
-            msg = String::null();
-        }
-        return CPtr(new StatusImpl(code, msg));
-    }
-
-    static CPtr create(Int code, String::CPtr msg) {
-        return CPtr(new StatusImpl(code, msg));
-    }
-
- private:
-    Int code_;
-    String::CPtr message_;
-
-    StatusImpl(Int code, String::CPtr msg)
-        : code_(code)
-        , message_(msg) {}
-};
-
 Status::CPtr Status::create(Int code) {
-    return StatusImpl::create(code);
+    LIBJ_STATIC_SYMBOL_DEF(symOK, "OK");
+
+    String::CPtr msg;
+    if (code == OK) {
+        msg = symOK;
+    } else {
+        msg = String::null();
+    }
+    return CPtr(new detail::Status<Status>(code, msg));
 }
 
 Status::CPtr Status::create(Int code, String::CPtr msg) {
-    return StatusImpl::create(code, msg);
+    return CPtr(new detail::Status<Status>(code, msg));
 }
 
 }  // namespace libj
