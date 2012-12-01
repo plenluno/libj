266e3fbc96553a9ff14d9e8e052f01d3f03c8c0a
Plenluno <plenluno@zoho.com>
2012-12-01 10:35:06 +0900

    revive JsTypedArray for compatibility

---
+++ include/libj/js_typed_array.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2012 Plenluno All rights reserved.
+
+#ifndef LIBJ_JS_TYPED_ARRAY_H_
+#define LIBJ_JS_TYPED_ARRAY_H_
+
+#include <libj/typed_js_array.h>
+
+// for compatibility
+#define JsTypedArray TypedJsArray
+
+#endif  // LIBJ_JS_TYPED_ARRAY_H_
