99ac54011e79bb0dcaf5f7584733b244184761ed
Plenluno <plenluno@zoho.com>
2013-03-27 00:17:25 +0900

    add SIGNALING_NAN, POSITIVE_INFINITY and NEGATIVE_INFINITY

--- src/constant.cpp
+++ src/constant.cpp
@@ -12,10 +12,14 @@ const Size NO_SIZE = -1;
 
 const Char NO_CHAR = -1;
 
-const Double INFINITY = std::numeric_limits<Double>::infinity();
+const Value UNDEFINED;
 
 const Double QUIET_NAN = std::numeric_limits<Double>::quiet_NaN();
 
-const Value UNDEFINED;
+const Double SIGNALING_NAN = std::numeric_limits<Double>::signaling_NaN();
+
+const Double POSITIVE_INFINITY = std::numeric_limits<Double>::infinity();
+
+const Double NEGATIVE_INFINITY = -std::numeric_limits<Double>::infinity();
 
 }  // namespace libj
