30467ec984608edf43c52c93dba8cddb91d46016
Plenluno <plenluno@zoho.com>
2013-05-03 09:16:26 +0900

    make glue::RegExp::execute thread-safe

--- src/glue/regexp.cpp
+++ src/glue/regexp.cpp
@@ -78,6 +78,7 @@ class RegExpImpl : public RegExpBase {
         int* cs = new int[len];
         int r = dart::jscre::jsRegExpExecute(
             re_, subject.c_str(), subject.length(), offset, cs, len);
+        captures.clear();
         for (int i = 0; i < num; i++) {
             captures.push_back(cs[i * 2]);
             captures.push_back(cs[i * 2 + 1]);
@@ -123,13 +124,13 @@ class RegExpImpl : public RegExpBase {
         const std::string& str,
         int offset,
         std::vector<int>& captures) const {
-        static iv::aero::VM vm;
-        captures.clear();
         assert(code_);
         size_t n = code_->captures() * 2;
+        captures.clear();
         for (size_t i = 0; i < n; i++) {
             captures.push_back(-1);
         }
+        iv::aero::VM vm;
         int res = vm.Execute(code_, toU16String(str), captures.data(), offset);
         return res == iv::aero::AERO_SUCCESS;
     }
