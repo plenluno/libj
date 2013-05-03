e025495a3474278a0c3b7c5411169bdcd98dd666
Plenluno <plenluno@zoho.com>
2013-05-04 01:01:03 +0900

    print leaked objects with call stacks

--- tools/leak-check.rb
+++ tools/leak-check.rb
@@ -5,27 +5,43 @@ require 'set'
 prevLine = ""
 numObjects = 0
 
+callStack = []
+callStacks = {}
+
 objects = Set.new
 staticObjects = Set.new
 
 STDIN.each do |line|
-  if /^\[LIBJ DEBUG\] construct: 0x(\w*)/ =~ line
+  if /\[LIBJ TRACE\] enter: (\[.*\])/ =~ line
+    callStack.push($1)
+  end
+
+  if /\[LIBJ TRACE\] exit:  (\[.*\])/ =~ line
+    call = callStack.pop
+    if call != $1
+      raise "not enter: " + $1
+    end
+  end
+
+  if /\[LIBJ DEBUG\] construct: 0x(\w*)/ =~ line
     if objects.include?($1)
       raise "already constructed: " + $1
     else
       objects.add($1)
+      callStacks[$1] = callStack.inspect
     end
   end
 
-  if /^\[LIBJ DEBUG\] destruct:  0x(\w*)/ =~ line
+  if /\[LIBJ DEBUG\] destruct:  0x(\w*)/ =~ line
     if !objects.include?($1)
 #     warn "not constructed: " + $1
     else
       objects.delete($1)
+      callStacks.delete($1)
     end
   end
 
-  if /^\[LIBJ DEBUG\] static: (.*) 0x(\w*)/ =~ line
+  if /\[LIBJ DEBUG\] static: (.*) 0x(\w*)/ =~ line
     if staticObjects.include?($2)
       raise "already constructed: " + $2
     else
@@ -33,7 +49,7 @@ STDIN.each do |line|
     end
   end
 
-  if /^\[LIBJ DEBUG\] symbol: \[.*\] 0x(\w*)/ =~ line
+  if /\[LIBJ DEBUG\] symbol: \[.*\] 0x(\w*)/ =~ line
     if staticObjects.include?($1)
       raise "already constructed: " + $1
     else
@@ -41,7 +57,7 @@ STDIN.each do |line|
     end
   end
 
-  if  /^\[LIBJ DEBUG\] symbol: \[.*/ =~ prevLine and /^\] 0x(\w*)/ =~ line
+  if  /\[LIBJ DEBUG\] symbol: \[.*/ =~ prevLine and /^\] 0x(\w*)/ =~ line
     if staticObjects.include?($1)
       raise "already constructed: " + $1
     else
@@ -49,7 +65,7 @@ STDIN.each do |line|
     end
   end
 
-  if /^\[LIBJ DEBUG\] remaining objects: (\d*)/ =~ line
+  if /\[LIBJ DEBUG\] remaining objects: (\d*)/ =~ line
     numObjects = $1
     break;
   end
@@ -62,6 +78,9 @@ if objects.size != numObjects.to_i
 end
 
 leakedObjects = objects ^ staticObjects
-if !leakedObjects.empty?
-  raise "leaked objects: " + leakedObjects.inspect
+
+leakedObjects.each do |addr|
+  puts "leaked object: " + addr
+  puts callStacks[addr]
+  puts
 end
