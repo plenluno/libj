f7ae267f9034ca0857d218053ced34c669708762
Plenluno <plenluno@zoho.com>
2013-05-13 22:58:12 +0900

    include console.h only if LIBJ_DEBUG is ON

--- include/libj/debug_print.h
+++ include/libj/debug_print.h
@@ -3,9 +3,8 @@
 #ifndef LIBJ_DEBUG_PRINT_H_
 #define LIBJ_DEBUG_PRINT_H_
 
-#include <libj/console.h>
-
 #ifdef LIBJ_DEBUG
+# include <libj/console.h>
 # define LIBJ_DEBUG_PRINT(FMT, ...) \
     libj::console::printf( \
         libj::console::LEVEL_DEBUG, \
