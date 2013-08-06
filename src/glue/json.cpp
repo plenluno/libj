5012aca32ab8a01f33c357941ce81780c72cc3ca
Plenluno <plenluno@zoho.com>
2013-08-06 21:50:35 +0900

    use rapidjson

--- src/glue/json.cpp
+++ src/glue/json.cpp
@@ -1,12 +1,14 @@
-// Copyright (c) 2012 Plenluno All rights reserved.
+// Copyright (c) 2012-2013 Plenluno All rights reserved.
 
 #include <libj/error.h>
+#include <libj/endian.h>
 #include <libj/js_array.h>
 #include <libj/js_object.h>
 #include <libj/glue/json.h>
 
-#include <picojson.h>
 #include <math.h>
+#include <rapidjson/document.h>
+
 #include <string>
 #include <sstream>
 
@@ -14,10 +16,10 @@ namespace libj {
 namespace glue {
 namespace json {
 
-static const Double JSON_INTEGER_MAX = +9007199254740991.0;
-static const Double JSON_INTEGER_MIN = -9007199254740991.0;
-
 static Value doubleToValue(Double d) {
+    static const Double JSON_INTEGER_MAX = +9007199254740991.0;
+    static const Double JSON_INTEGER_MIN = -9007199254740991.0;
+
     Double intpart;
     if (modf(d, &intpart) == 0.0 &&
         intpart <= JSON_INTEGER_MAX &&
@@ -29,33 +31,73 @@ static Value doubleToValue(Double d) {
     }
 }
 
-static Value convert(const picojson::value& val) {
-    if (val.is<picojson::null>()) {
+static Value longToValue(Long l) {
+    static const Long JSON_INTEGER_MAX = +9007199254740991;
+    static const Long JSON_INTEGER_MIN = -9007199254740991;
+
+    if (l <= JSON_INTEGER_MAX && l >= JSON_INTEGER_MIN) {
+        return l;
+    } else {
+        return static_cast<Double>(l);
+    }
+}
+
+static Boolean isBigEndian() {
+    static Endian e = endian();
+    return e == BIG;
+}
+
+static String::CPtr createString(const Char* s) {
+#ifdef LIBJ_USE_UTF32
+    if (isBigEndian()) {
+        return String::create(s, String::UTF32BE);
+    } else {
+        return String::create(s, String::UTF32LE);
+    }
+#else
+    if (isBigEndian()) {
+        return String::create(s, String::UTF16BE);
+    } else {
+        return String::create(s, String::UTF16LE);
+    }
+#endif
+}
+
+#ifdef LIBJ_USE_UTF32
+typedef rapidjson::UTF32<Char> JsonEnc;
+#else
+typedef rapidjson::UTF16<Char> JsonEnc;
+#endif
+
+typedef rapidjson::GenericValue<JsonEnc> JsonValue;
+typedef rapidjson::GenericDocument<JsonEnc> Document;
+
+static Value convert(const JsonValue& val) {
+    if (val.IsNull()) {
         return Object::null();
-    } else if (val.is<Boolean>()) {
-        return val.get<Boolean>();
-    } else if (val.is<Double>()) {
-        return doubleToValue(val.get<Double>());
-    } else if (val.is<std::string>()) {
-        std::string s = val.get<std::string>();
-        return String::create(s.c_str(), String::UTF8);
-    } else if (val.is<picojson::array>()) {
-        picojson::array pa = val.get<picojson::array>();
+    } else if (val.IsBool()) {
+        return val.GetBool();
+    } else if (val.IsInt64()) {
+        return longToValue(val.GetInt64());
+    } else if (val.IsDouble()) {
+        return doubleToValue(val.GetDouble());
+    } else if (val.IsString()) {
+        return createString(val.GetString());
+    } else if (val.IsArray()) {
         JsArray::Ptr ja = JsArray::create();
-        for (picojson::array::const_iterator i = pa.begin();
-             i != pa.end();
-             ++i) {
-            ja->add(convert(*i));
+        for (JsonValue::ConstValueIterator itr = val.Begin();
+             itr != val.End();
+             ++itr) {
+            ja->add(convert(*itr));
         }
         return ja;
-    } else if (val.is<picojson::object>()) {
-        picojson::object po = val.get<picojson::object>();
+    } else if (val.IsObject()) {
         JsObject::Ptr jo = JsObject::create();
-        for (picojson::object::const_iterator i = po.begin();
-             i != po.end();
-             ++i) {
-            String::CPtr k = String::create(i->first.c_str());
-            Value v = convert(i->second);
+        for (JsonValue::ConstMemberIterator itr = val.MemberBegin();
+             itr != val.MemberEnd();
+             ++itr) {
+            String::CPtr k = createString(itr->name.GetString());
+            Value v = convert(itr->value);
             jo->put(k, v);
         }
         return jo;
@@ -67,15 +109,11 @@ static Value convert(const picojson::value& val) {
 Value parse(String::CPtr str) {
     if (!str) return Error::create(Error::ILLEGAL_ARGUMENT);
 
-    picojson::value v;
-    std::string s = str->toStdString();
-    const char* cstr = s.c_str();
-    std::string err;
-    picojson::parse(v, cstr, cstr + s.length(), &err);
-    if (err.empty()) {
-        return convert(v);
-    } else {
+    Document doc;
+    if (doc.Parse<0>(str->data()).HasParseError()) {
         return Error::create(Error::ILLEGAL_ARGUMENT);
+    } else {
+        return convert(doc);
     }
 }
 
