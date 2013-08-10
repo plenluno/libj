47f6875f9b22bf01d8df1fa24ae89c38d58083f9
Plenluno <plenluno@zoho.com>
2013-08-10 16:28:27 +0900

    parse JSON in SAX style

--- src/glue/json.cpp
+++ src/glue/json.cpp
@@ -41,11 +41,21 @@ static Value longToValue(Long l) {
     }
 }
 
-static String::CPtr createString(const Char* s) {
+static Value ulongToValue(ULong ul) {
+    static const Long JSON_INTEGER_MAX = +9007199254740991;
+
+    if (ul <= JSON_INTEGER_MAX) {
+        return static_cast<Long>(ul);
+    } else {
+        return static_cast<Double>(ul);
+    }
+}
+
+static String::CPtr createString(const Char* s, Size len) {
 #ifdef LIBJ_USE_UTF32
-    return String::create(s, String::UTF32);
+    return String::create(s, String::UTF32, len);
 #else
-    return String::create(s, String::UTF16);
+    return String::create(s, String::UTF16, len);
 #endif
 }
 
@@ -55,51 +65,141 @@ typedef rapidjson::UTF32<Char> JsonEnc;
 typedef rapidjson::UTF16<Char> JsonEnc;
 #endif
 
-typedef rapidjson::GenericValue<JsonEnc> JsonValue;
-typedef rapidjson::GenericDocument<JsonEnc> Document;
+typedef rapidjson::GenericStringStream<JsonEnc> JsonStream;
+typedef rapidjson::GenericReader<JsonEnc, JsonEnc> JsonReader;
 
-static Value convert(const JsonValue& val) {
-    if (val.IsNull()) {
-        return Object::null();
-    } else if (val.IsBool()) {
-        return val.GetBool();
-    } else if (val.IsInt64()) {
-        return longToValue(val.GetInt64());
-    } else if (val.IsDouble()) {
-        return doubleToValue(val.GetDouble());
-    } else if (val.IsString()) {
-        return createString(val.GetString());
-    } else if (val.IsArray()) {
-        JsArray::Ptr ja = JsArray::create();
-        for (JsonValue::ConstValueIterator itr = val.Begin();
-             itr != val.End();
-             ++itr) {
-            ja->add(convert(*itr));
-        }
-        return ja;
-    } else if (val.IsObject()) {
-        JsObject::Ptr jo = JsObject::create();
-        for (JsonValue::ConstMemberIterator itr = val.MemberBegin();
-             itr != val.MemberEnd();
-             ++itr) {
-            String::CPtr k = createString(itr->name.GetString());
-            Value v = convert(itr->value);
-            jo->put(k, v);
-        }
-        return jo;
-    } else {
-        return UNDEFINED;
+class JsonHandler {
+ public:
+    JsonHandler()
+        : stack_(JsArray::create())
+        , array_(JsArray::null())
+        , object_(JsObject::null())
+        , name_(String::null()) {}
+
+    Value root() {
+        return root_;
     }
-}
+
+    void add(const Value& v) {
+        if (array_) {
+            array_->add(v);
+        } else if (object_) {
+            assert(name_);
+            object_->put(name_, v);
+            name_ = String::null();
+        } else {
+            assert(root_.isUndefined());
+            root_ = v;
+        }
+    }
+
+    void push() {
+        if (array_) {
+            stack_->push(array_);
+            array_ = JsArray::null();
+        } else if (object_) {
+            stack_->push(object_);
+            object_ = JsObject::null();
+        }
+    }
+
+    void pop() {
+        if (stack_->isEmpty()) return;
+
+        Value v = stack_->pop();
+        JsArray::Ptr ary = toPtr<JsArray>(v);
+        if (ary) {
+            array_ = ary;
+        } else {
+            JsObject::Ptr obj = toPtr<JsObject>(v);
+            assert(obj);
+            object_ = obj;
+        }
+    }
+
+    void Null()  {
+        add(Object::null());
+    }
+
+    void Bool(bool b)  {
+        add(b);
+    }
+
+    void Int(int i) {
+        add(static_cast<Long>(i));
+    }
+
+    void Uint(unsigned ui)  {
+        add(static_cast<Long>(ui));
+    }
+
+    void Int64(int64_t l) {
+        add(longToValue(l));
+    }
+
+    void Uint64(uint64_t ul) {
+        add(ulongToValue(ul));
+    }
+
+    void Double(double d) {
+        add(doubleToValue(d));
+    }
+
+    void String(const Char* s, rapidjson::SizeType len, bool copy) {
+        String::CPtr str = createString(s, len);
+        if (object_ && !name_) {
+            name_ = str;
+        } else {
+            add(str);
+        }
+    }
+
+    void StartObject() {
+        JsObject::Ptr obj = JsObject::create();
+        add(obj);
+        push();
+        object_ = obj;
+    }
+
+    void EndObject(rapidjson::SizeType size) {
+        assert(object_->size() == size);
+        object_ = JsObject::null();
+        pop();
+    }
+
+    void StartArray() {
+        JsArray::Ptr ary = JsArray::create();
+        add(ary);
+        push();
+        array_ = ary;
+    }
+
+    void EndArray(rapidjson::SizeType len) {
+        assert(array_->length() == len);
+        array_ = JsArray::null();
+        pop();
+    }
+
+ private:
+    Value root_;
+    JsArray::Ptr stack_;
+    JsArray::Ptr array_;
+    JsObject::Ptr object_;
+    String::CPtr name_;
+};
 
 Value parse(String::CPtr str) {
     if (!str) return Error::create(Error::ILLEGAL_ARGUMENT);
 
-    Document doc;
-    if (doc.Parse<0>(str->data()).HasParseError()) {
+    JsonReader reader;
+    JsonStream stream(str->data());
+    JsonHandler handler;
+    const unsigned int parseFlags = rapidjson::kParseDefaultFlags;
+    reader.Parse<parseFlags, JsonStream, JsonHandler>(stream, handler);
+    if (reader.HasParseError()) {
         return Error::create(Error::ILLEGAL_ARGUMENT);
     } else {
-        return convert(doc);
+        return handler.root();
     }
 }
 
