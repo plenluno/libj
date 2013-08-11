53da69f866456e71f93682caeb430929dcf12e75
Plenluno <plenluno@zoho.com>
2013-08-11 20:06:57 +0900

    add -runtime/int to --filter

--- tools/cpplint.sh
+++ tools/cpplint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-tools/cpplint/cpplint.py --filter=-runtime/explicit,-runtime/references,-runtime/sizeof,-readability/streams,-build/include_order,-build/include_what_you_use --prefix=libj `find include src btest gtest -type f`
+tools/cpplint/cpplint.py --filter=-runtime/int,-runtime/explicit,-runtime/references,-runtime/sizeof,-readability/streams,-build/include_order,-build/include_what_you_use --prefix=libj `find include src btest gtest -type f`
