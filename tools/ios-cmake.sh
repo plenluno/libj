084fd6327480dba1796375f6eda0286eef5fb60a
Plenluno <plenluno@zoho.com>
2013-10-05 15:08:37 +0900

    pass arguments to ios-cmake

--- tools/ios-cmake.sh
+++ tools/ios-cmake.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p build.ios
 cd build.ios
-cmake -DCMAKE_TOOLCHAIN_FILE=`dirname $0`/../tools/ios-cmake/toolchain/iOS.cmake -DCMAKE_BUILD_TYPE=Release -GXcode ..
+cmake -DCMAKE_TOOLCHAIN_FILE=`dirname $0`/../tools/ios-cmake/toolchain/iOS.cmake -DCMAKE_BUILD_TYPE=Release -GXcode $@ ..
 xcodebuild -configuration Release
