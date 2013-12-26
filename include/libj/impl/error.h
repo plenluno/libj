772c9c2debb4e06cd234c51dfb0cfabb96cfc088
plenluno <plenluno@zoho.com>
2013-12-27 00:20:12 +0900

    add hash<libj::Error::Code>

---
+++ include/libj/impl/error.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2013 Plenluno All rights reserved.
+
+#ifndef LIBJ_IMPL_ERROR_H_
+#define LIBJ_IMPL_ERROR_H_
+
+#ifdef LIBJ_USE_CXX11
+namespace std {
+#else
+namespace boost {
+#endif
+    template <>
+    struct hash<libj::Error::Code> {
+        std::size_t operator()(const libj::Error::Code& key) const {
+            return static_cast<std::size_t>(key);
+        }
+    };
+}
+
+#endif  // LIBJ_IMPL_ERROR_H_
