9a0d267e6fbb3d3e4cdec54963ebe96a10c1b286
Plenluno <plenluno@zoho.com>
2013-12-29 18:54:14 +0900

    add scripts to switch between gcc and clang

---
+++ tools/update-alternatives-clang.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo update-alternatives --set cc  /usr/bin/clang
+sudo update-alternatives --set c++ /usr/bin/clang++
---
+++ tools/update-alternatives-gcc.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo update-alternatives --set cc  /usr/bin/gcc
+sudo update-alternatives --set c++ /usr/bin/g++
