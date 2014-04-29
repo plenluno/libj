5ad78556a3a79376eb1ae3effce2ac5af95cb122
Plenluno <plenluno@zoho.com>
2014-04-29 18:08:35 +0900

    refactor the path of CMAKE_TOOLCHAIN_FILE

--- tools/android-cmake.sh
+++ tools/android-cmake.sh
@@ -2,5 +2,5 @@
 # export ANDROID_NDK=/path/to/android-ndk
 mkdir -p build.android
 cd build.android
-cmake -DCMAKE_TOOLCHAIN_FILE=`dirname $0`/../tools/android-cmake/toolchain/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-4.8 -DANDROID_NATIVE_API_LEVEL=android-9 -DANDROID_ABI=armeabi $@ ..
+cmake -DCMAKE_TOOLCHAIN_FILE=`dirname $0`/android-cmake/toolchain/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-4.8 -DANDROID_NATIVE_API_LEVEL=android-9 -DANDROID_ABI=armeabi $@ ..
 make
